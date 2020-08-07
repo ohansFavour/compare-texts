3e958fe153958499b1af7483a427de5fe9721a5e
Favour Ohans <fohanekwu@gmail.com>
2020-08-07 16:48:24 +0100

    Initialize project using Create React App

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+ const HomePage:React.FC = ():JSX.Element => {
+    return (
+        <div className="homepage">
+            
+        </div>
+    )
+}
+
+export default HomePage
